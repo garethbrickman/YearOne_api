3d01542c91d28b9e2589721f11530d123df64940
Gareth Brickman <brickstick@gmail.com>
2020-11-19 22:55:58 -0500

    [FIX] Correct user credentials and db name

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  USER: "YearOne",
+  PASSWORD: "@YearOne",
+  DB: "films_testdb",
   dialect: "mysql",
   pool: {
     max: 5,
